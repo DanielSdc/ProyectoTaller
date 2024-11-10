522e1b5d3d5037003852f95f54942bf75720a1c8
Jorge <jorgescalante3@gmail.com>
2024-11-09 21:00:12 -0700

    Modificacion del Main

--- js/login.js
+++ js/login.js
@@ -2,5 +2,5 @@ const form = document.getElementById("loginForm");
 
 form.addEventListener("submit", function(event) {
     event.preventDefault(); 
-    window.location.href = "../pages/mainlogeado.html";  
+    window.location.href = "../pages/main.html";  
 });
