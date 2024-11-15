13e35d7ab04709be3bc4e7955469afa966b209e0
Daniel Castro <L21170281@culiacan.tecnm.mx>
2024-11-14 20:05:38 -0700

    Auth login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,23 @@
-const form = document.getElementById("loginForm");
 
-form.addEventListener("submit", function(event) {
-    event.preventDefault(); 
-    window.location.href = "../pages/main.html";  
-});
+import { auth, signInWithEmailAndPassword } from '../firebaseconfig.js';
+
+  document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+
+    const user = document.getElementById('user').value;
+    const password = document.getElementById('password').value;
+
+    signInWithEmailAndPassword(auth,user, password)
+      .then((userCredential) => {
+        // Inicio de sesión exitoso
+        const user = userCredential.user;
+        console.log('Usuario autenticado:', user);
+        // Redirigir o realizar alguna acción
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error('Error al iniciar sesión:', errorCode, errorMessage);
+      });
+  });
