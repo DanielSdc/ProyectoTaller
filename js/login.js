ce2b3d8c9db063805886d9e85759394a6965c6fb
Jorge <jorgescalante3@gmail.com>
2024-11-16 21:31:37 -0700

    Mensaje de credenciales LOGIN

--- js/login.js
+++ js/login.js
@@ -1,25 +1,32 @@
-
 import { auth, signInWithEmailAndPassword } from '../firebaseconfig.js';
 
-  document.getElementById('loginForm').addEventListener('submit', function(event) {
+document.getElementById('loginForm').addEventListener('submit', function (event) {
     event.preventDefault();
 
-
+    // Obtener los valores de los campos de usuario y contraseña
     const user = document.getElementById('user').value;
     const password = document.getElementById('password').value;
 
-    signInWithEmailAndPassword(auth,user, password)
-      .then((userCredential) => {
-        // Inicio de sesión exitoso
-        const user = userCredential.user;
-        console.log('Usuario autenticado:', user);
-        // Redirigir o realizar alguna acción
-        window.location.href = '../pages/main.html';
-        
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.error('Error al iniciar sesión:', errorCode, errorMessage);
-      });
-  });
+    // Limpiar mensaje de error previo (si existe)
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.classList.add('d-none');
+
+    // Intentar iniciar sesión
+    signInWithEmailAndPassword(auth, user, password)
+        .then((userCredential) => {
+            // Inicio de sesión exitoso
+            const user = userCredential.user;
+            console.log('Usuario autenticado:', user);
+            // Redirigir al usuario a la página principal
+            window.location.href = '../pages/main.html';
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.error('Error al iniciar sesión:', errorCode, errorMessage);
+
+            // Mostrar el mensaje de error
+            const errorMessageElement = document.getElementById('error-message');
+            errorMessageElement.classList.remove('d-none'); // Hacer visible el mensaje
+        });
+});
