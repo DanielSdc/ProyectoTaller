23656aae087385c9bc0c8cffbecf003827df8ee5
Gilberto Felix <gilfelixgg00@gmail.com>
2024-11-21 19:44:37 -0700

    Nombre Inmueble en rentas

--- js/tablaventas.js
+++ js/tablaventas.js
@@ -1,3 +1,5 @@
+import { auth, db, collection, query, where, getDocs } from './firebaseconfig.js';
+
 $(document).ready(function () {
   const table = $("#inmueblesTable").DataTable();
 
@@ -27,4 +29,35 @@ $(document).ready(function () {
     $("#inmuebleForm")[0].reset();
     $("#addInmuebleModal").modal("hide");
   });
+});
+
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const inmuebleSelect = document.getElementById('inmueble');
+
+    // Escuchar los cambios en el estado de autenticación
+    auth.onAuthStateChanged(async (user) => {
+        if (user) {
+            try {
+                const q = query(
+                    collection(db, "inmuebles"),
+                    where("usuarioId", "==", user.uid)
+                );
+                const querySnapshot = await getDocs(q);
+
+                querySnapshot.forEach((doc) => {
+                    const data = doc.data();
+                    const option = document.createElement('option');
+                    option.value = data.nombre; // Usar el ID del inmueble
+                    option.textContent = data.nombre; // Mostrar el nombre del inmueble
+                    inmuebleSelect.appendChild(option);
+                });
+            } catch (error) {
+                console.error("Error al cargar inmuebles:", error);
+            }
+        } else {
+            console.warn("Usuario no autenticado. No se pueden cargar inmuebles.");
+        }
+    });
 });
